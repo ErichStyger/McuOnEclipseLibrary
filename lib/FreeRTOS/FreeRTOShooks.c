0aa9e7066ef02ccf11977a623c047071a54056e7
Erich Styger <erich.styger@hslu.ch>
2019-03-16 07:41:42 +0100

    more support for RISC-V

--- lib/FreeRTOS/FreeRTOShooks.c
+++ lib/FreeRTOS/FreeRTOShooks.c
@@ -5,6 +5,7 @@
  *      Author: Erich Styger
  */
 
+#include "McuLibconfig.h"
 #include "FreeRTOS.h"
 #include "task.h"
 /*
@@ -111,13 +112,15 @@ void McuRTOS_vApplicationIdleHook(void)
 void McuRTOS_vOnPreSleepProcessing(TickType_t expectedIdleTicks)
 {
   (void)expectedIdleTicks; /* not used */
-#if 1
-  /* example for Kinetis (enable SetOperationMode() in CPU component): */
+#if McuLib_CONFIG_CPU_IS_ARM_CORTEX_M
+  /* example for ARM Cortex-M (enable SetOperationMode() in CPU component): */
   // Cpu_SetOperationMode(DOM_WAIT, NULL, NULL); /* Processor Expert way to get into WAIT mode */
   /* or to wait for interrupt: */
     __asm volatile("dsb");
     __asm volatile("wfi");
     __asm volatile("isb");
+#elif McuLib_CONFIG_CPU_IS_RISC_V
+  #warning "NYI" /* \todo */
 #elif 0
   /* example for S08/S12/ColdFire V1 (enable SetWaitMode() in CPU): */
   Cpu_SetWaitMode();
