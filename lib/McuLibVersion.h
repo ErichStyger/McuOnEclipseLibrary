6a944d53c3825f21a41cb52d4f7d4e4b2786503e
Erich Styger <erich.styger@hslu.ch>
2019-05-08 10:26:39 +0200

    removed M33 related file from M4 port

--- lib/McuLibVersion.h
+++ lib/McuLibVersion.h
@@ -1,22 +1,15 @@
-/*
- * McuLibVersion.h
- *
- *  Created on: 20.04.2019
- *      Author: Erich Styger
- */
-
 #ifndef SOURCES_MCULIBVERSION_H_
 #define SOURCES_MCULIBVERSION_H_
 
-/* Date: 20-Apr-2019
- * Time: 13:20
+/* Date: 07-May-2019
+ * Time: 21:23
  */
 
 #define McuLib_VERSION_VERSION_MAJOR   (1)
   /*!< McuLib major version number */
 #define McuLib_VERSION_VERSION_MINOR   (0)
   /*!< McuLib minor version number */
-#define McuLib_VERSION_VERSION_BUILD   (0)
+#define McuLib_VERSION_VERSION_BUILD   (1)
   /*!< McuLib build version number */
 
 #endif /* SOURCES_MCULIBVERSION_H_ */
