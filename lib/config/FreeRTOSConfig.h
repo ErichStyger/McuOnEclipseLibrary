f957daf90356449081db00009241fc72724c60c5
Erich Styger <erich.styger@hslu.ch>
2020-07-28 16:28:00 +0200

    support for LPTMR

--- lib/config/FreeRTOSConfig.h
+++ lib/config/FreeRTOSConfig.h
@@ -90,8 +90,12 @@
 #ifndef configTICK_RATE_HZ
   #define configTICK_RATE_HZ                      (1000) /* frequency of tick interrupt */
 #endif
-#define configSYSTICK_USE_LOW_POWER_TIMER         0 /* If using Kinetis Low Power Timer (LPTMR) instead of SysTick timer */
-#define configSYSTICK_LOW_POWER_TIMER_CLOCK_HZ    1 /* 1 kHz LPO timer. Set to 1 if not used */
+#ifndef configSYSTICK_USE_LOW_POWER_TIMER
+  #define configSYSTICK_USE_LOW_POWER_TIMER       0 /* If using Kinetis Low Power Timer (LPTMR) instead of SysTick timer */
+#endif
+#ifndef configSYSTICK_LOW_POWER_TIMER_CLOCK_HZ
+  #define configSYSTICK_LOW_POWER_TIMER_CLOCK_HZ  1 /* Frequency of low power timer. Set to 1 if not used */
+#endif
 #if McuLib_CONFIG_NXP_SDK_USED || McuLib_CONFIG_SDK_VERSION_USED==McuLib_CONFIG_SDK_GENERIC || McuLib_CONFIG_SDK_VERSION_USED==McuLib_CONFIG_SDK_NORDIC_NRF5
 /* The CMSIS variable SystemCoreClock contains the current clock speed */
   extern uint32_t SystemCoreClock;
