4ad252360acd41950bb2959264ee664ca064b7b4
Erich Styger <erich.styger@hslu.ch>
2020-07-28 16:30:30 +0200

    supress warnings

--- lib/SEGGER_Sysview/SEGGER_SYSVIEW.c
+++ lib/SEGGER_Sysview/SEGGER_SYSVIEW.c
@@ -637,14 +637,14 @@ static void _SendPacket(U8* pStartPacket, U8* pEndPacket, unsigned int EventId) 
 
 #if (SEGGER_SYSVIEW_POST_MORTEM_MODE == 1)
   if (_SYSVIEW_Globals.EnableState == 0) {
-    goto SendDone;
+    goto SendDone; /* @suppress("Goto statement used") */
   }
 #else
   if (_SYSVIEW_Globals.EnableState == 1) {  // Enabled, no dropped packets remaining
-    goto Send;
+    goto Send; /* @suppress("Goto statement used") */
   }
   if (_SYSVIEW_Globals.EnableState == 0) {
-    goto SendDone;
+    goto SendDone; /* @suppress("Goto statement used") */
   }
   //
   // Handle buffer full situations:
@@ -654,7 +654,7 @@ static void _SendPacket(U8* pStartPacket, U8* pEndPacket, unsigned int EventId) 
   if (_SYSVIEW_Globals.EnableState == 2) {
     _TrySendOverflowPacket();
     if (_SYSVIEW_Globals.EnableState != 1) {
-      goto SendDone;
+      goto SendDone; /* @suppress("Goto statement used") */
     }
   }
 Send:
@@ -664,7 +664,7 @@ Send:
   //
   if (EventId < 32) {
     if (_SYSVIEW_Globals.DisabledEvents & ((U32)1u << EventId)) {
-      goto SendDone;
+      goto SendDone; /* @suppress("Goto statement used") */
     }
   }
   //
