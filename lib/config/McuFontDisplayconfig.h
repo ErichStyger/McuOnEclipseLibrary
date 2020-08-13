b50b9c2cfc73da37385b4707982850cd4def2bb1
Erich Styger <erich.styger@hslu.ch>
2020-08-13 20:05:31 +0200

    added more copyright information

--- lib/config/McuFontDisplayconfig.h
+++ lib/config/McuFontDisplayconfig.h
@@ -1,5 +1,11 @@
-#ifndef __McuFontDisplay_CONFIG_H
-#define __McuFontDisplay_CONFIG_H
+/**
+ * \file
+ * \brief Configuration header file for FontDisplay
+ * Copyright (c) 2020, Erich Styger
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This header file is used to configure settings of the Font Display module.
+ */
 
 /* no configuration supported yet */
 
