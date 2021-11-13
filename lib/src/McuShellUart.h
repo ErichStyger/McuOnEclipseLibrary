6b241e1bb10ea9aab923b233591b823a4ee537d6
Erich Styger <erich.styger@hslu.ch>
2021-11-13 19:58:33 +0100

    added interface documentation

--- lib/src/McuShellUart.h
+++ lib/src/McuShellUart.h
@@ -14,10 +14,20 @@
 extern "C" {
 #endif
 
+/*! Default McuShell buffer */
 extern uint8_t McuShellUart_DefaultShellBuffer[McuShell_DEFAULT_SHELL_BUFFER_SIZE]; /* default buffer which can be used by the application */
+
+/*! Standard I/O for input/output */
 extern McuShell_ConstStdIOType McuShellUart_stdio;
 
+/*!
+ * \brief Module de-initialization
+ */
 void McuShellUart_Deinit(void);
+
+/*!
+ * \brief Module initialization
+ */
 void McuShellUart_Init(void);
 
 #ifdef __cplusplus
