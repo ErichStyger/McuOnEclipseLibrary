c513fbba78392e0744cf7e1124704d7feeb793dd
Erich Styger <erich.styger@hslu.ch>
2024-05-07 15:49:18 +0200

    added scaling factor to NOP execution time

--- lib/config/McuWaitconfig.h
+++ lib/config/McuWaitconfig.h
@@ -1,7 +1,7 @@
 /**
  * \file
  * \brief Configuration header file for Wait
- * Copyright (c) 2020, Erich Styger
+ * Copyright (c) 2020-2024, Erich Styger
  * SPDX-License-Identifier: BSD-3-Clause
  *
  * This header file is used to configure settings of the busy waiting module.
@@ -15,6 +15,11 @@
 #ifndef McuWait_CONFIG_USE_CYCLE_COUNTER
   #define McuWait_CONFIG_USE_CYCLE_COUNTER  (1 && (McuLib_CONFIG_CPU_IS_ARM_CORTEX_M && McuLib_CONFIG_CORTEX_M>=3))
     /*!< 1: Use hardware cycle counter (if present, only on Cortex-M3 or higher), 0: not using hardware cycle counter */
+#endif
+
+#ifndef McuWait_CONFIG_NOF_CYCLES_FOR_NOP
+  #define McuWait_CONFIG_NOF_CYCLES_FOR_NOP   (1)
+	/*!< by default, use a single cycle for a NOP instruction. Some devices like the LPC804 need 2 cycles */
 #endif
 
 #ifndef McuWait_CONFIG_USE_RTOS_WAIT
