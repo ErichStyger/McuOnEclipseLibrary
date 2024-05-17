18449d105b17adab52d00a32ed7b06b39ee370f6
Erich Styger <erich.styger@hslu.ch>
2024-05-17 19:06:11 +0200

    added ability to scale the number of nops needed

--- lib/config/McuWaitconfig.h
+++ lib/config/McuWaitconfig.h
@@ -17,9 +17,14 @@
     /*!< 1: Use hardware cycle counter (if present, only on Cortex-M3 or higher), 0: not using hardware cycle counter */
 #endif
 
-#ifndef McuWait_CONFIG_NOF_CYCLES_FOR_NOP
-  #define McuWait_CONFIG_NOF_CYCLES_FOR_NOP   (1)
-	/*!< by default, use a single cycle for a NOP instruction. Some devices like the LPC804 need 2 cycles */
+#ifndef McuWait_CONFIG_NOF_CYCLES_FOR_NOP_MUL
+  #define McuWait_CONFIG_NOF_CYCLES_FOR_NOP_MUL   (1)
+	  /*!< by default, use a single cycle for a NOP instruction. Some devices like the LPC804 need more. This factor gets it multiplied, see McuWait_CONFIG_NOF_CYCLES_FOR_NOP_DIV. */
+#endif
+
+#ifndef McuWait_CONFIG_NOF_CYCLES_FOR_NOP_DIV
+  #define McuWait_CONFIG_NOF_CYCLES_FOR_NOP_DIV   (1)
+    /*!< by default, use a single cycle for a NOP instruction. Some devices like the LPC804 need more. This factor gets it divided, see McuWait_CONFIG_NOF_CYCLES_FOR_NOP_MUL. */
 #endif
 
 #ifndef McuWait_CONFIG_USE_RTOS_WAIT
