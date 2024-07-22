f9e83fe3b1cd637d411bab14fbcd8d7054b1728b
Erich Styger <erich.styger@hslu.ch>
2024-07-22 08:38:50 +0200

    updated coverage support

--- lib/config/McuCoverageconfig.h
+++ lib/config/McuCoverageconfig.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2024, Erich Styger
  *
  * SPDX-License-Identifier: BSD-3-Clause
- * \file
+ *
  * \brief Configuration header file for the McuCoverage module.
  */
 
@@ -10,9 +10,13 @@
 #define MCUCOVERAGECONFIG_H_
 
 #ifndef McuCoverage_CONFIG_IS_ENABLED
-  #define McuCoverage_CONFIG_IS_ENABLED            (1)
+  #define McuCoverage_CONFIG_IS_ENABLED               (1)
     /*!< 1: Module is enabled; 0: Module is disabled, not adding anything to the application code */
 #endif
 
+#ifndef McuCoverage_CONFIG_USE_STANDALONE
+  #define McuCoverage_CONFIG_USE_STANDALONE            (1)
+    /*!< 1: Implementation of standalone version; 0: No standalone support */
+#endif
 
 #endif /* MCUCOVERAGECONFIG_H_ */
